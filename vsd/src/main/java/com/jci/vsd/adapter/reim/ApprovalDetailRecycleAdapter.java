15c2a0ec1c101ba8e9d9dc516bee80ef8ade02cf
liqing_1938 <2318241264@qq.com>
2018-07-27 10:20:27 +0800

    审批单列表详情完成3

--- vsd/src/main/java/com/jci/vsd/adapter/reim/ApprovalDetailRecycleAdapter.java
+++ vsd/src/main/java/com/jci/vsd/adapter/reim/ApprovalDetailRecycleAdapter.java
@@ -88,6 +88,7 @@ public class ApprovalDetailRecycleAdapter extends RecyclerView.Adapter<ApprovalD
                     holder.ivPic.setImageBitmap(listBitmap.get(position));
                 }
 
+
                 holder.ivPic.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
